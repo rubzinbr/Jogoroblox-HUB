2b4c00089e3433b17047ae8c50650b9a877f2525
rubzinbr <jogofreefirepromax@gmail.com>
2025-09-06 12:40:29 -0300

    Delete musica.lua

--- musica.lua
+++
@@ -1,9 +0,0 @@
-
--- Música de fundo
-local SoundService = game:GetService("SoundService")
-local musica = Instance.new("Sound", SoundService)
-musica.Name = "JogorobloxMusica"
-musica.SoundId = "rbxassetid://1837436066" -- música alternativa
-musica.Volume = 1
-musica.Looped = true
-musica:Play()
