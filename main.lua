c58b2a145e9742589250136ccef30c46a9650d16
rubzinbr <jogofreefirepromax@gmail.com>
2025-09-06 12:49:31 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,10 +1,3 @@
+local repo = "https://raw.githubusercontent.com/rubzinbr/Jogoroblox-HUB/main/"
 
--- Jogoroblox HUB Loader
-local repo = "https://raw.githubusercontent.com/hubzinbr/Jogoroblox-HUB/main/"
-
--- Tela de carregamento
-loadstring(game:HttpGet(repo .. "carregamento.lua"))()
-task.wait(2)
-
--- Sistema de chave
-loadstring(game:HttpGet(repo .. "key.lua"))()
+loadstring(game:HttpGet(repo .. "main.lua"))()
