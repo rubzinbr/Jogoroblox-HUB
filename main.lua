c18fe42151956bc5174ddf230ca99c0cbb16e2b1
rubzinbr <jogofreefirepromax@gmail.com>
2025-09-06 12:54:03 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
 local repo = "https://raw.githubusercontent.com/rubzinbr/Jogoroblox-HUB/main/"
 
-loadstring(game:HttpGet(repo .. "main.lua"))()
+-- função para carregar os módulos direto do GitHub
+local function loadModule(name)
+    local url = repo .. name .. ".lua"
+    return loadstring(game:HttpGet(url))()
+end
+
+-- carrega os módulos
+local Interface = loadModule("interface")
+local Botoes = loadModule("botoes")
+
+-- cria interface
+local contentFrame = Interface.Criar()
+
+-- adiciona os botões na interface
+Botoes.Criar(contentFrame)
